214f54b314b7ec06756138a189d90793a01f37c6
Mark McRee <mmcree@cableone.net>
2018-11-04 10:14:12 -0600

    Added readme file.  Added comments to Factorial.java.

--- Factorial.java
+++ Factorial.java
@@ -1,5 +1,14 @@
 import java.io.*;
 
+/*
+ * A simple class that is used to calculate the facotiral
+ * of a number.  To calculate the factorial of a given 
+ * number n one must multiply n * n-1 * n-2....n*0.  In
+ * example, factorial(5) = 5 * 4 * 3 * 2 * 1 which is
+ * equal to 120.
+ * @author Mark McRee
+ */
+
 public class Factorial{
 
 	public Factorial(int n){
---
+++ README.txt
@@ -0,0 +1,3 @@
+A git repository of Java8 files.  The repository serves two purposes:
+1.  Practice programming in Java8.
+2.  Practice remote git operations to a from the repository.
