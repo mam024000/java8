c1d74de6584a39f527c8292594482e857901ccb6
Mark McRee <mmcree@cableone.net>
2018-11-11 17:15:59 -0600

    java8-001: Added flower garden to Hello.java.

--- Hello.java
+++ Hello.java
@@ -1,5 +1,14 @@
 import java.io.*;
 
+/*
+ * A simple program to show the initialization process in a Java8 program.
+ * A new Hello object is instantiated.  The constructor is called first.
+ * The constructor prints a message.  It then makes a call to the method
+ * printMessage() which prints another message.  The control is returned
+ * to main() which prints the last message.
+ * @author Mark
+ */
+
 public class Hello{
 
 	public Hello(){
@@ -13,7 +22,7 @@ public class Hello{
 
 	public static void main(String[] args){
 		Hello app = new Hello();
-		System.out.println("Hello world!");
+		System.out.println("Hello world from main!");
 	}
 
 }
